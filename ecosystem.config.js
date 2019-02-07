1afa48f8d251b5c4160c9c248cac89857e09db28
AmauryMBA <amaury.tobiasqr@gmail.com>
2019-02-07 11:56:57 -0600

    prettierrc and eslintrc

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,18 @@
 module.exports = {
-    apps: [{
-        name: "blog",
-        script: "hexo server"
-    }],
-    "deploy": {
-        "production": {
-            "user": "amaurytobias",
-            "host": "amaurytq.me",
-            "ref": "origin/master",
-            "repo": "git@github.com:amaury-tobias/blog.git",
-            "path": "/home/amaurytobias/production/blog",
-            "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
-        },
+  apps: [
+    {
+      name: 'blog',
+      script: 'hexo server'
     }
+  ],
+  deploy: {
+    production: {
+      user: 'amaurytobias',
+      host: 'amaurytq.me',
+      ref: 'origin/master',
+      repo: 'git@github.com:amaury-tobias/blog.git',
+      path: '/home/amaurytobias/production/blog',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
 }
